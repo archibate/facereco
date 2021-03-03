a33f8e0e679f140fbca76f8027c22af06bf7a11d
archibate <1931127624@qq.com>
2021-03-03 16:03:52 +0800

    3d face landmarks

--- parse.py
+++ parse.py
@@ -9,11 +9,12 @@ with open('/tmp/233.pkl', 'rb') as f:
 
 pos = []
 for i in range(100):
-     if f'x_{i}' not in res:
+     if f'X_{i}' not in res:
          break
-     pos.append((res[f'x_{i}'], res[f'y_{i}']))
+     pos.append((res[f'X_{i}'], res[f'Y_{i}'], res[f'Z_{i}']))
 assert len(pos)
 pos = np.array(pos)
 
-plt.scatter(pos[:, 0], pos[:, 1])
+ax = plt.figure().add_subplot(111, projection='3d')
+ax.scatter(pos[:, 0], pos[:, 1], pos[:, 2], c='r', marker='^')
 plt.show()
