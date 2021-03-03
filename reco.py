420c1a46f57f6a1e75aa19bfefdb49cb26c8e447
archibate <1931127624@qq.com>
2021-03-03 15:53:18 +0800

    reco face landmarks

--- reco.py
+++ reco.py
@@ -1,10 +1,10 @@
-#__import__('IPython').embed()
 import os
 import csv
 import shutil
 import docker
 import tempfile
 import numpy as np
+import matplotlib.pyplot as plt
 
 def process_image(imgpath):
     with tempfile.TemporaryDirectory() as tmpdir:
@@ -25,32 +25,24 @@ def process_image(imgpath):
             rows = [[x.strip() for x in row] for row in csv.reader(f)]
             assert len(rows) == 2
             res = dict((k, float(v)) for k, v in zip(*rows))
+            keys = rows[0]
 
-    return res, rows[0]
-
-
-def get_image_ident(imgpath):
-    print('processing', imgpath)
-    res, keys = process_image(imgpath)
-    keys = 'AU01_r', 'AU02_r', 'AU04_r', 'AU05_r', 'AU06_r', 'AU07_r', 'AU09_r', 'AU10_r', 'AU12_r', 'AU14_r', 'AU15_r', 'AU17_r', 'AU20_r', 'AU23_r', 'AU25_r', 'AU26_r', 'AU45_r', 'AU01_c', 'AU02_c', 'AU04_c', 'AU05_c', 'AU06_c', 'AU07_c', 'AU09_c', 'AU10_c', 'AU12_c', 'AU14_c', 'AU15_c', 'AU17_c', 'AU20_c', 'AU23_c', 'AU25_c', 'AU26_c', 'AU28_c', 'AU45_c'
-    ret = []
-    for k in keys:
-        ret.append(res[k])
-    return np.array(ret)
+    return res, keys
 
 
-def mse(x, y):
-    return np.linalg.norm(x - y)
+res, keys = process_image('233.jpg')
 
 
-a = get_image_ident('001.jpg')
-b = get_image_ident('030.jpg')
-c = get_image_ident('233.jpg')
-d = get_image_ident('666.jpg')
 
-print('001=030', mse(a, b))
-print('233=666', mse(c, d))
-print('001-233', mse(a, c))
-print('030-233', mse(b, c))
-print('001-666', mse(a, d))
-print('030-666', mse(b, d))
+pos = []
+for i in range(100):
+     if f'x_{i}' not in res:
+         break
+     pos.append((res[f'x_{i}'], res[f'y_{i}']))
+pos = np.array(pos)
+
+plt.scatter(pos[:, 0], pos[:, 1])
+plt.show()
+
+
+#__import__('IPython').embed()
