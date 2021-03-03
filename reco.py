f77b02144da536ade622712fa0d9556a89326909
archibate <1931127624@qq.com>
2021-03-03 14:54:34 +0800

    use official api

--- reco.py
+++ reco.py
@@ -1,7 +1,11 @@
+#__import__('IPython').embed()
 import os, docker
 C = docker.from_env()
 
-ret = C.containers.run('algebr/openface:latest', '-c "ls -l"')
-print(ret.decode())
+def wrapcmd(x):
+    return f'-c "{x}"'
 
-#__import__('IPython').embed()
+ret = C.containers.run('algebr/openface',
+        wrapcmd('echo hello world'),
+        detach=True)
+print(ret.logs().decode())
