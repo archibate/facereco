65c185f1a6b7d7c75d14d9030ffa0fa157a2c259
archibate <1931127624@qq.com>
2021-03-03 14:57:50 +0800

    mount volumes

--- reco.py
+++ reco.py
@@ -2,10 +2,18 @@
 import os, docker
 C = docker.from_env()
 
+try:
+    os.mkdir('/tmp/share')
+except IOError:
+    pass
+
 def wrapcmd(x):
     return f'-c "{x}"'
 
 ret = C.containers.run('algebr/openface',
-        wrapcmd('echo hello world'),
-        detach=True)
+        wrapcmd('echo a > /mnt/share/a.txt'),
+        detach=True,
+        volumes={
+            '/tmp/share': dict(bind='/mnt/share', mode='rw'),
+        })
 print(ret.logs().decode())
