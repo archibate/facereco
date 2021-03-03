b3b399929053726a4f6faf37b1488502ed372318
archibate <1931127624@qq.com>
2021-03-03 15:38:31 +0800

    failed 666 test

--- reco.py
+++ reco.py
@@ -26,15 +26,15 @@ def process_image(imgpath):
             assert len(rows) == 2
             res = dict((k, float(v)) for k, v in zip(*rows))
 
-    return res
+    return res, rows[0]
 
 
 def get_image_ident(imgpath):
     print('processing', imgpath)
-    res = process_image(imgpath)
-    AUs = 'AU01_r', 'AU02_r', 'AU04_r', 'AU05_r', 'AU06_r', 'AU07_r', 'AU09_r', 'AU10_r', 'AU12_r', 'AU14_r', 'AU15_r', 'AU17_r', 'AU20_r', 'AU23_r', 'AU25_r', 'AU26_r', 'AU45_r', 'AU01_c', 'AU02_c', 'AU04_c', 'AU05_c', 'AU06_c', 'AU07_c', 'AU09_c', 'AU10_c', 'AU12_c', 'AU14_c', 'AU15_c', 'AU17_c', 'AU20_c', 'AU23_c', 'AU25_c', 'AU26_c', 'AU28_c', 'AU45_c'
+    res, keys = process_image(imgpath)
+    keys = 'AU01_r', 'AU02_r', 'AU04_r', 'AU05_r', 'AU06_r', 'AU07_r', 'AU09_r', 'AU10_r', 'AU12_r', 'AU14_r', 'AU15_r', 'AU17_r', 'AU20_r', 'AU23_r', 'AU25_r', 'AU26_r', 'AU45_r', 'AU01_c', 'AU02_c', 'AU04_c', 'AU05_c', 'AU06_c', 'AU07_c', 'AU09_c', 'AU10_c', 'AU12_c', 'AU14_c', 'AU15_c', 'AU17_c', 'AU20_c', 'AU23_c', 'AU25_c', 'AU26_c', 'AU28_c', 'AU45_c'
     ret = []
-    for k in AUs:
+    for k in keys:
         ret.append(res[k])
     return np.array(ret)
 
@@ -46,7 +46,11 @@ def mse(x, y):
 a = get_image_ident('001.jpg')
 b = get_image_ident('030.jpg')
 c = get_image_ident('233.jpg')
+d = get_image_ident('666.jpg')
 
-print('001 030', mse(a, b))
-print('001 233', mse(a, c))
-print('030 233', mse(b, c))
+print('001=030', mse(a, b))
+print('233=666', mse(c, d))
+print('001-233', mse(a, c))
+print('030-233', mse(b, c))
+print('001-666', mse(a, d))
+print('030-666', mse(b, d))
