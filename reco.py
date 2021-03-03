a9622df3ad2fad263f94f75eba49f5e85312d7bb
archibate <1931127624@qq.com>
2021-03-03 15:36:11 +0800

    add 233 test

--- reco.py
+++ reco.py
@@ -4,6 +4,7 @@ import csv
 import shutil
 import docker
 import tempfile
+import numpy as np
 
 def process_image(imgpath):
     with tempfile.TemporaryDirectory() as tmpdir:
@@ -29,15 +30,23 @@ def process_image(imgpath):
 
 
 def get_image_ident(imgpath):
+    print('processing', imgpath)
     res = process_image(imgpath)
     AUs = 'AU01_r', 'AU02_r', 'AU04_r', 'AU05_r', 'AU06_r', 'AU07_r', 'AU09_r', 'AU10_r', 'AU12_r', 'AU14_r', 'AU15_r', 'AU17_r', 'AU20_r', 'AU23_r', 'AU25_r', 'AU26_r', 'AU45_r', 'AU01_c', 'AU02_c', 'AU04_c', 'AU05_c', 'AU06_c', 'AU07_c', 'AU09_c', 'AU10_c', 'AU12_c', 'AU14_c', 'AU15_c', 'AU17_c', 'AU20_c', 'AU23_c', 'AU25_c', 'AU26_c', 'AU28_c', 'AU45_c'
     ret = []
     for k in AUs:
         ret.append(res[k])
-    return ret
+    return np.array(ret)
 
 
-ret1 = get_image_ident('001.jpg')
-print(ret1)
-ret2 = get_image_ident('030.jpg')
-print(ret2)
+def mse(x, y):
+    return np.linalg.norm(x - y)
+
+
+a = get_image_ident('001.jpg')
+b = get_image_ident('030.jpg')
+c = get_image_ident('233.jpg')
+
+print('001 030', mse(a, b))
+print('001 233', mse(a, c))
+print('030 233', mse(b, c))
