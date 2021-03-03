a65ad005398de55e9ac6c6508c04151512f0d3c5
archibate <1931127624@qq.com>
2021-03-03 15:21:00 +0800

    record AUs

--- reco.py
+++ reco.py
@@ -16,10 +16,15 @@ ret = C.containers.run('algebr/openface',
         volumes={
             '/tmp/share': dict(bind='/mnt/share', mode='rw'),
         }, auto_remove=True)
-print(ret.decode())
+#print(ret.decode())
 
 with open('/tmp/share/result.csv') as f:
     rows = [[x.strip() for x in row] for row in csv.reader(f)]
     assert len(rows) == 2
     res = dict((k, float(v)) for k, v in zip(*rows))
-    print(res)
+
+AUs = 'AU01_r', 'AU02_r', 'AU04_r', 'AU05_r', 'AU06_r', 'AU07_r', 'AU09_r', 'AU10_r', 'AU12_r', 'AU14_r', 'AU15_r', 'AU17_r', 'AU20_r', 'AU23_r', 'AU25_r', 'AU26_r', 'AU45_r', 'AU01_c', 'AU02_c', 'AU04_c', 'AU05_c', 'AU06_c', 'AU07_c', 'AU09_c', 'AU10_c', 'AU12_c', 'AU14_c', 'AU15_c', 'AU17_c', 'AU20_c', 'AU23_c', 'AU25_c', 'AU26_c', 'AU28_c', 'AU45_c'
+ret = []
+for k in AUs:
+    ret.append(res[k])
+print(ret)
