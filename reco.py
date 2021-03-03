e49bf86ad8b0bbd81c05e118f830d58c20b2d65a
archibate <1931127624@qq.com>
2021-03-03 15:22:52 +0800

    refactor

--- reco.py
+++ reco.py
@@ -1,22 +1,22 @@
 #__import__('IPython').embed()
 import os, shutil, docker, csv
-C = docker.from_env()
+
+input_image = './001.jpg'
 
 shutil.rmtree('/tmp/share', ignore_errors=True)
 os.mkdir('/tmp/share')
-
-shutil.copyfile('./001.jpg', '/tmp/share/image.jpg')
+shutil.copyfile(input_image, '/tmp/share/image.jpg')
 
 def wrapcmd(x):
     return f'-c "{x}"'
 
-ret = C.containers.run('algebr/openface',
+C = docker.from_env()
+stdout = C.containers.run('algebr/openface',
         wrapcmd('build/bin/FaceLandmarkVidMulti -f /mnt/share/image.jpg'
                 ' && cp processed/image.csv /mnt/share/result.csv'),
         volumes={
             '/tmp/share': dict(bind='/mnt/share', mode='rw'),
         }, auto_remove=True)
-#print(ret.decode())
 
 with open('/tmp/share/result.csv') as f:
     rows = [[x.strip() for x in row] for row in csv.reader(f)]
