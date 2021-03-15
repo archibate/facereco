ce89e271ad752d99f838bcc3190d9278fa219756
archibate <1931127624@qq.com>
2021-03-15 17:07:46 +0800

    parallel using joblib

--- facereco/facereco.py
+++ facereco/facereco.py
@@ -7,47 +7,52 @@ import pickle
 import time
 
 '''
-pip3 install opencv-python face_recognition imutils numpy dlib
+pip3 install opencv-python face_recognition imutils joblib numpy dlib
 '''
 
-class FaceDetector:
-    def __init__(self, data=(), shift=0):
-        self.data = list(data)
-        self.shift = shift
+def encode(img, shift=0):
+    rgb_down = rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    for i in range(shift):
+        rgb_down = cv2.pyrDown(rgb_down)
+    boxes = list(face_recognition.face_locations(rgb_down))
+    for i, (top, right, bottom, left) in enumerate(boxes):
+        top <<= shift
+        right <<= shift
+        bottom <<= shift
+        left <<= shift
+        boxes[i] = top, right, bottom, left
+    encodings = face_recognition.face_encodings(rgb, boxes)
+    return encodings
 
-    def encode(self, img):
-        rgb_down = rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        for i in range(self.shift):
-            rgb_down = cv2.pyrDown(rgb_down)
-        boxes = list(face_recognition.face_locations(rgb_down))
-        for i, (top, right, bottom, left) in enumerate(boxes):
-            top <<= self.shift
-            right <<= self.shift
-            bottom <<= self.shift
-            left <<= self.shift
-            boxes[i] = top, right, bottom, left
-        encodings = face_recognition.face_encodings(rgb, boxes)
-        return encodings
+def detect(data, img, shift=0):
+    encodings = encode(img)
+    for encoding in encodings:
+        matches = face_recognition.compare_faces(data, encoding)
+        if any(matches):
+            return True
+    return False
 
-    def detect(self, img):
-        encodings = self.encode(img)
-        for encoding in encodings:
-            matches = face_recognition.compare_faces(self.data, encoding)
-            if any(matches):
-                return True
-        return False
-
-    def train(self, images):
-        self.data = []
-        images = list(images)
+def train(images, parallel=False):
+    data = []
+    images = list(images)
+    t0 = time.time()
+    if parallel:
+        import joblib
+        import multiprocessing
+        n_jobs = min(multiprocessing.cpu_count(), len(images))
+        parallel = joblib.Parallel(n_jobs=n_jobs)
+        iterator = parallel(joblib.delayed(encode)(img) for img in images)
+        for encodings in iterator:
+            data.extend(encodings)
+    else:
         for i, img in enumerate(images):
-            print('training {}/{}...'.format(i + 1, len(images)))
-            encodings = self.encode(img)
-            self.data.extend(encodings)
-        return self.data
+            print('training {}/{}'.format(i + 1, len(images)))
+            encodings = encode(img)
+            data.extend(encodings)
+    print(time.time() - t0, 'secs')
+    return data
 
 
 if __name__ == '__main__':
-    dec = FaceDetector()
-    dec.train(map(cv2.imread, imutils.paths.list_images('dataset/linus/')))
-    print(dec.detect(cv2.imread('images/linus.jpg')))
+    data = train(map(cv2.imread, imutils.paths.list_images('dataset/linus/')))
+    print(detect(data, cv2.imread('images/linus.jpg')))
